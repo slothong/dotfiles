0a26854f80f6a31544ffa7dec22b42829eaff865
slothong <hep93@naver.com>
2025-09-29 23:45:43 +0900

    telescope delete_buffer 단축키 수정

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -5,5 +5,22 @@ return {
     local builtin = require("telescope.builtin")
     vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
     vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+    vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+
+    require("telescope").setup({
+
+      defaults = {
+        mappings = {
+          n = {
+            ["<c-d>"] = require("telescope.actions").delete_buffer,
+          }, -- n
+          i = {
+            ["<c-d>"] = require("telescope.actions").delete_buffer,
+            ["dd"] = require("telescope.actions").delete_buffer,
+          },
+        },
+      },
+    })
   end,
+  opts = {},
 }

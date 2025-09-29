a3a07107178060bfdea31d8979d48d3cd40f9541
slothong <hep93@naver.com>
2025-09-29 23:02:12 +0900

    lazygit

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 vim.keymap.set("i", "jk", "<ESC>", { noremap = true })
 vim.keymap.set("i", "kj", "<ESC>", { noremap = true })
-
+vim.keymap.set(
+  "n",
+  "<leader>gg",
+  "<cmd>!tmux new-window -c " .. vim.fn.getcwd() .. " -- lazygit <CR><CR>",
+  { desc = "Git Go" }
+) -- opens lazygit in a new tmux window

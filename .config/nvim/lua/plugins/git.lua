280eba21e0caac46442d98500f71d4eb42302e2e
slothong <hep93@naver.com>
2025-09-30 21:38:18 +0900

    gitsigns

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -7,4 +7,27 @@ return {
       vim.keymap.set("n", "<leader>dh", "<cmd>DiffviewFileHistory<CR>", { silent = true })
     end,
   },
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      current_line_blame = true,
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+        delay = 300,
+        ignore_whitespace = false,
+      },
+      on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+        map("n", "<leader>hb", function()
+          gs.blame_line({ full = true })
+        end)
+      end,
+    },
+  },
 }

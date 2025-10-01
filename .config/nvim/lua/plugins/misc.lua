71683b4291ef4f67cefeb64c1b0f78fe6121cdae
slothong <hep93@naver.com>
2025-10-01 09:21:48 +0900

    add autopair

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -2,4 +2,9 @@ return {
   "nvim-lua/plenary.nvim",
   "tpope/vim-surround",
   { "windwp/nvim-ts-autotag", opts = {} },
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = true,
+  },
 }
